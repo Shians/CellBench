cfa48f2e326db5d2dad1f15eeb1d0ad4b07b0002
Shians <registertonysu@gmail.com>
2018-10-18 16:41:24 +1100

    Updated knn_smoothing k parameter selection

--- R/analysis_impute.R
+++ R/analysis_impute.R
@@ -1,22 +1,19 @@
 impute_drimpute <- function(data) {
     expr <- lib_size_norm_expr(data)
     expr_processed <- DrImpute::preprocessSC(expr)
-    DrImpute::DrImpute(expr_processed)
+    count_mat <- DrImpute::DrImpute(expr_processed)
+    count_mat
 }
 
 impute_knn_smooth <- function(data) {
     source("https://raw.githubusercontent.com/yanailab/knn-smoothing/master/knn_smooth.R")
-    expr <- lib_size_norm_expr(data)
-    knn_smoothing(mat = expr, k = 32)
+    expr <- counts(data)
+    k <- ceiling(ncol(data) / 20)
+    count_mat <- knn_smoothing(mat = expr, k = k) %>% lib_size_norm_expr()
+    count_mat
 }
 
 impute_basics <- function(data) {
-    counts <- counts(data) %>% filter_zero_genes()
-    tech_features <- stringr::str_detect(rownames(Counts), "ERCC")
-    tech_counts <- counts[tech_features, ]
-    spike_info <- data.frame("SpikeID" = rownames(Counts)[tech_features], "SpikeInput" = row_apply(tech_counts, max))
-
-    data <- BASiCS::newBASiCS_Data(counts, tech_features, spike_info)
-    chain <- BASiCS::BASiCS_MCMC(data, N = 50, Thin = 2, Burn = 10, Regression = TRUE, PrintProgress = FALSE)
-    BASiCS::BASiCS_DenoisedCounts(Data = data, Chain = chain)
+    count_mat <- run_basics(data)
+    count_mat %>% lib_size_norm_expr()
 }
