c0058fd4561a88addca849e4081ee0aa302b3cac
Shians <registertonysu@gmail.com>
2018-10-18 16:42:11 +1100

    Added alias for is_one_of

--- R/utils_check.R
+++ R/utils_check.R
@@ -3,6 +3,7 @@ is_one_of <- function(x, classes) {
     stopifnot(is(classes, "character"))
     purrr::map_lgl(classes, function(class) is(x, class)) %>% any()
 }
+is_any_of <- is_one_of
 
 # check if object is all of a vector classes
 is_all_of <- function(x, classes) {
