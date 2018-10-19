c8bd95e6d1cb97b3045487b65e3fd5794b8bb61f
Shians <registertonysu@gmail.com>
2018-10-19 11:26:53 +1100

    Added more filtering functions

--- R/utils_sampling.R
+++ R/utils_sampling.R
@@ -1,3 +1,6 @@
+# alias for subsetting operator for code clarity elsewhere
+subset_inds <- `[`
+
 # sample n rows from data
 sample_rows <- function(x, n) {
     if (nrow(x) > n) {
@@ -26,4 +29,36 @@ sample_cells <- function(x, n) {
 sample_genes <- function(x, n) {
     stopifnot(is(x, "SingleCellExperiment"))
     sample_rows(x, n)
+}
+
+# filter down to highest expressed genes
+keep_high_count_genes <- function(x, n) {
+    stopifnot(is(x, "SingleCellExperiment"))
+    highest <- rowSums(SingleCellExperiment::counts(x)) %>%
+        order(decreasing = TRUE) %>%
+        subset_inds(seq_len(n))
+
+    x[highest, ]
+}
+
+# filter down to largest samples
+keep_high_count_cells <- function(x, n) {
+    stopifnot(is(x, "SingleCellExperiment"))
+    highest <- colSums(SingleCellExperiment::counts(x)) %>%
+        order(decreasing = TRUE) %>%
+        subset_inds(seq_len(n))
+
+    x[, highest]
+}
+
+# filter down to highest expressed genes
+keep_high_var_genes <- function(x, n) {
+    stopifnot(is(x, "SingleCellExperiment"))
+    counts <- SingleCellExperiment::counts(x)
+    scaled_var <- row_apply(counts, var) / rowSums(counts)
+    highest <- scaled_var %>%
+        order(decreasing = TRUE) %>%
+        `[`(seq_len(n))
+
+    x[highest, ]
 }
