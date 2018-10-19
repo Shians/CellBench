bb822a6637020e1cab2540bc2e60dfdd2bf98917
Shians <registertonysu@gmail.com>
2018-10-19 11:35:07 +1100

    Moved filtering functions from sampling to filter utils

--- R/utils_filter.R
+++ R/utils_filter.R
@@ -1,3 +1,4 @@
+# filter out genes that have zero expression in all samples
 filter_zero_genes <- function(x) {
     stopifnot(is_one_of(x, c("SingleCellExperiment", "matrix")))
 
@@ -8,4 +9,36 @@ filter_zero_genes <- function(x) {
     }
 
     x[!zero_genes, ]
+}
+
+# filter down to highest expressed genes
+keep_high_count_genes <- function(x, n) {
+    stopifnot(is(x, "SingleCellExperiment"))
+    highest <- rowSums(SingleCellExperiment::counts(x)) %>%
+        order(decreasing = TRUE) %>%
+        subset_inds(seq_len(n))
+
+    x[highest, ]
+}
+
+# filter down to largest samples
+keep_high_count_cells <- function(x, n) {
+    stopifnot(is(x, "SingleCellExperiment"))
+    highest <- colSums(SingleCellExperiment::counts(x)) %>%
+        order(decreasing = TRUE) %>%
+        subset_inds(seq_len(n))
+
+    x[, highest]
+}
+
+# filter down to highest expressed genes
+keep_high_var_genes <- function(x, n) {
+    stopifnot(is(x, "SingleCellExperiment"))
+    counts <- SingleCellExperiment::counts(x)
+    scaled_var <- row_apply(counts, var) / rowSums(counts)
+    highest <- scaled_var %>%
+        order(decreasing = TRUE) %>%
+        `[`(seq_len(n))
+
+    x[highest, ]
 }
--- R/utils_sampling.R
+++ R/utils_sampling.R
@@ -30,35 +30,3 @@ sample_genes <- function(x, n) {
     stopifnot(is(x, "SingleCellExperiment"))
     sample_rows(x, n)
 }
-
-# filter down to highest expressed genes
-keep_high_count_genes <- function(x, n) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    highest <- rowSums(SingleCellExperiment::counts(x)) %>%
-        order(decreasing = TRUE) %>%
-        subset_inds(seq_len(n))
-
-    x[highest, ]
-}
-
-# filter down to largest samples
-keep_high_count_cells <- function(x, n) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    highest <- colSums(SingleCellExperiment::counts(x)) %>%
-        order(decreasing = TRUE) %>%
-        subset_inds(seq_len(n))
-
-    x[, highest]
-}
-
-# filter down to highest expressed genes
-keep_high_var_genes <- function(x, n) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    counts <- SingleCellExperiment::counts(x)
-    scaled_var <- row_apply(counts, var) / rowSums(counts)
-    highest <- scaled_var %>%
-        order(decreasing = TRUE) %>%
-        `[`(seq_len(n))
-
-    x[highest, ]
-}
