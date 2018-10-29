d18721a813af4febef195c63dfd0242f7b4494c9
Shians <registertonysu@gmail.com>
2018-10-29 15:50:18 +1100

    Added parallelism to method application

--- R/apply_methods.R
+++ R/apply_methods.R
@@ -1,31 +1,43 @@
-apply_methods <- function(x, fn_list) {
+apply_methods <- function(x, fn_list, .name) {
     UseMethod("apply_methods", x)
 }
 
 # create data x methods cross product
 # returns list of (data_name, method_name, result) values
-apply_methods.list <- function(data_list, fn_list) {
+apply_methods.list <- function(data_list, fn_list, .name) {
     d_names <- names(data_list)
     m_names <- names(fn_list)
-
-    output <- list()
-    for (d_name in d_names) {
-        for (m_name in m_names) {
-            result <- list(
-                data_list = d_name,
-                .temp = m_name,
-                result = fn_list[[m_name]](data_list[[d_name]])
-            )
-            output <- append(output, list(result))
-        }
+    if (length(m_names) != length(fn_list)) {
+        stop("every element of fn_list must be named")
     }
+
+    if (missing(".name")) {
+        .name <- deparse(substitute(fn_list))
+    }
+
+    output <- BiocParallel::bplapply(
+        d_names,
+        function(d_name) {
+            purrr::map(
+                m_names,
+                function(m_name) {
+                    result <- list(
+                        data_list = d_name,
+                        .temp = m_name,
+                        result = fn_list[[m_name]](data_list[[d_name]])
+                    )
+                    list(result)
+                }
+            ) %>% purrr::reduce(append)
+        }
+    ) %>% purrr::reduce(append)
 
     output <- tibble::tibble(
         data = purrr::map_chr(output, function(x) x$data),
         .temp = purrr::map_chr(output, function(x) x$.temp),
         result = purrr::map(output, function(x) x$result)
     )
-    names(output)[2] <- deparse(substitute(fn_list))
+    names(output)[2] <- .name
 
     if (all_length_one(output$result)) {
         output$result <- unlist(output$result)
@@ -40,8 +52,12 @@ all_length_one <- function(x) {
     all(sapply(x, function(x) { length(x) == 1 }))
 }
 
-apply_methods.tbl_df <- function(tbl_df, fn_list) {
+apply_methods.tbl_df <- function(tbl_df, fn_list, .name) {
     m_names <- names(fn_list)
+
+    if (missing(".name")) {
+        .name <- deparse(substitute(fn_list))
+    }
 
     results <- list()
     for (data in tbl_df$result) {
@@ -52,7 +68,7 @@ apply_methods.tbl_df <- function(tbl_df, fn_list) {
 
     output <- tbl_df %>% select(-result)
     output <- tidyr::crossing(tbl_df, names(fn_list))
-    names(output)[ncol(output)] <- deparse(substitute(fn_list))
+    names(output)[ncol(output)] <- .name
     output <- output %>%
         dplyr::mutate(result = results) %>%
         dplyr::select(-result, result) # move result column to end
