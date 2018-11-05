470b42347b618e432acf8cd3a7a484db31c5aa4f
Shians <registertonysu@gmail.com>
2018-11-06 10:46:10 +1100

    Added scran normalisation

--- R/analysis_normalise.R
+++ R/analysis_normalise.R
@@ -1,5 +1,5 @@
 # library size normalise
-lib_size_norm <- function(x) {
+.lib_size_norm <- function(x) {
     lib_sizes <- colSums(x)
     norm_factors <- lib_sizes / 1000000
     t(t(x) / norm_factors)
@@ -8,15 +8,30 @@ lib_size_norm <- function(x) {
 # library size normalise and transform to log scale
 lib_size_norm_expr <- function(x, log = TRUE, offset = 1) {
     stopifnot(is_one_of(x, c("matrix", "SingleCellExperiment")))
+
     if (is(x, "matrix")) {
         counts <- x
     } else {
         counts <- SingleCellExperiment::counts(x)
     }
-    expr <- lib_size_norm(counts)
+
+    expr <- .lib_size_norm(counts)
+
     if (log) {
-        base::log(expr + offset)
+        output <- base::log(expr + offset)
     } else {
-        expr
+        output <- expr
     }
+
+    output
+}
+
+# scran normalisation
+scran_norm_expr <- function(x, log = TRUE) {
+    stopifnot(is(x, "SingleCellExperiment"))
+
+    x <- scran::computeSumFactors(x)
+    x <- scater::normalize(x, log = log)
+
+    scater::norm_exprs(x)
 }
