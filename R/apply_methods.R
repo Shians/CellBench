8667886fac24fe43bf23812fe92d0fcf9ab91c39
Shians <registertonysu@gmail.com>
2018-11-07 17:27:58 +1100

    Added message suppression to apply_methods

--- R/apply_methods.R
+++ R/apply_methods.R
@@ -3,20 +3,37 @@
 #' @param x the list of data or benchmark tibble to apply methods to
 #' @param fn_list the list of methods to be applied
 #' @param .name (optional) the name of the column for methods applied
+#' @param suppress.messages TRUE if messages from running methods should be
+#'   suppressed
 #'
 #' @return benchmark_tbl object containing results from methods applied
 #' @export
 #'
 #' @examples
-apply_methods <- function(x, fn_list, .name = NULL) {
+#' # list of data
+#' datasets <- list(
+#'     set1 = rnorm(500, mean = 2, sd = 1),
+#'     set2 = rnorm(500, mean = 1, sd = 2)
+#' )
+#'
+#' # list of functions
+#' add_noise <- list(
+#'     none = identity,
+#'     add_bias = function(x) { x + 1 },
+#' )
+#'
+#' res <- apply_methods(datasets, add_noise)
+#'
+apply_methods <- function(x, fn_list, .name = NULL, suppress.messages = TRUE) {
     UseMethod("apply_methods", x)
 }
 
 # create data x methods cross product
 # returns list of (data_name, method_name, result) values
-apply_methods.list <- function(data_list, fn_list, .name = NULL) {
+apply_methods.list <- function(data_list, fn_list, .name = NULL, suppress.messages = TRUE) {
     d_names <- names(data_list)
     m_names <- names(fn_list)
+
     if (length(m_names) != length(fn_list)) {
         stop("every element of fn_list must be named")
     }
@@ -38,7 +55,7 @@ apply_methods.list <- function(data_list, fn_list, .name = NULL) {
                         result <- list(
                             data_list = d_name,
                             .temp = m_name,
-                            result = fn_list[[m_name]](data_list[[d_name]])
+                            result = suppressMessages(fn_list[[m_name]](data_list[[d_name]]))
                         )
                         list(result)
                     }
@@ -55,7 +72,7 @@ apply_methods.list <- function(data_list, fn_list, .name = NULL) {
                         result <- list(
                             data_list = d_name,
                             .temp = m_name,
-                            result = fn_list[[m_name]](data_list[[d_name]])
+                            result = suppressMessages(fn_list[[m_name]](data_list[[d_name]]))
                         )
                         list(result)
                     }
@@ -65,8 +82,8 @@ apply_methods.list <- function(data_list, fn_list, .name = NULL) {
     }
 
     output <- tibble::tibble(
-        data = purrr::map_chr(output, function(x) x$data),
-        .temp = purrr::map_chr(output, function(x) x$.temp),
+        data = factor_no_sort(purrr::map_chr(output, function(x) x$data)),
+        .temp = factor_no_sort(purrr::map_chr(output, function(x) x$.temp)),
         result = purrr::map(output, function(x) x$result)
     )
     names(output)[2] <- .name
@@ -84,7 +101,7 @@ all_length_one <- function(x) {
     all(sapply(x, function(x) { length(x) == 1 }))
 }
 
-apply_methods.tbl_df <- function(tbl_df, fn_list, .name = NULL) {
+apply_methods.tbl_df <- function(tbl_df, fn_list, .name = NULL, suppress.messages = TRUE) {
     m_names <- names(fn_list)
 
     if (missing(".name")) {
@@ -93,13 +110,13 @@ apply_methods.tbl_df <- function(tbl_df, fn_list, .name = NULL) {
 
     results <- list()
     for (data in tbl_df$result) {
-        for (fn in fn_list[order(names(fn_list))]) {
-            results <- append(results, list(fn(data)))
+        for (fn in fn_list) {
+            results <- append(results, list(suppressMessages(fn(data))))
         }
     }
 
     output <- tbl_df %>% select(-result)
-    output <- tidyr::crossing(tbl_df, names(fn_list))
+    output <- tidyr::crossing(tbl_df, factor_no_sort(m_names))
     names(output)[ncol(output)] <- .name
     output <- output %>%
         dplyr::mutate(result = results) %>%
@@ -113,5 +130,14 @@ apply_methods.tbl_df <- function(tbl_df, fn_list, .name = NULL) {
     output
 }
 
+#' @describeIn apply_methods
+#'
+#' @export
+#'
 apply_metrics <- apply_methods
+
+#' @describeIn apply_methods
+#'
+#' @export
+#'
 begin_benchmark <- apply_methods
