84fd226e1947f9627584e0db147e93aa887bac08
Shians <registertonysu@gmail.com>
2018-11-26 22:03:10 +1100

    Exported sampling functions

--- R/utils_sampling.R
+++ R/utils_sampling.R
@@ -1,7 +1,7 @@
 # sample n rows from data
 sample_rows <- function(x, n) {
     if (nrow(x) > n) {
-        x[sample(1:nrow(x), n), ]
+        x[sample(seq_nrow(x), n), ]
     } else {
         x
     }
@@ -10,18 +10,20 @@ sample_rows <- function(x, n) {
 # sample n cols from data
 sample_cols <- function(x, n) {
     if (ncol(x) > n) {
-        x[, sample(1:ncol(x), n)]
+        x[, sample(seq_ncol(x), n)]
     } else {
         x
     }
 }
 
+#' @export
 # sample n cells from SingleCellExperiment
 sample_cells <- function(x, n) {
     stopifnot(is(x, "SingleCellExperiment"))
     sample_cols(x, n)
 }
 
+#' @export
 # sample n genes from SingleCellExperiment
 sample_genes <- function(x, n) {
     stopifnot(is(x, "SingleCellExperiment"))
