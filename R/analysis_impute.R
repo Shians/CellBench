4c041dd7e153a908744bd1b65066c570b4db980f
Shians <registertonysu@gmail.com>
2018-11-26 22:04:49 +1100

    Updated imputation functions

--- R/analysis_impute.R
+++ R/analysis_impute.R
@@ -1,19 +1,21 @@
+#' @export
 impute_drimpute <- function(data) {
-    expr <- lib_size_norm_expr(data)
+    expr <- scran_norm_expr(data)
     expr_processed <- DrImpute::preprocessSC(expr)
     count_mat <- DrImpute::DrImpute(expr_processed)
     count_mat
 }
 
-impute_knn_smooth <- function(data) {
+#' @export
+impute_knn_smooth <- function(data, k = ceiling(ncol(data) / 20)) {
     source("https://raw.githubusercontent.com/yanailab/knn-smoothing/master/knn_smooth.R")
-    expr <- counts(data)
-    k <- ceiling(ncol(data) / 20)
+    expr <- scran_norm_expr(data)
     count_mat <- knn_smoothing(mat = expr, k = k) %>% lib_size_norm_expr()
     count_mat
 }
 
-impute_basics <- function(data) {
-    count_mat <- run_basics(data)
+#' @export
+impute_basics <- function(data, ...) {
+    count_mat <- run_basics(data, ...)
     log(count_mat + 1)
 }
