3e14f5f129f7fc1cef3d9626f29f8d4c7e378edc
Shians <registertonysu@gmail.com>
2018-11-30 00:54:06 +1100

    Added namespacing

--- R/utils_check.R
+++ R/utils_check.R
@@ -1,14 +1,14 @@
 # check if object is any of a vector classes
 is_one_of <- function(x, classes) {
-    stopifnot(is(classes, "character"))
-    purrr::map_lgl(classes, function(class) is(x, class)) %>% any()
+    stopifnot(methods::is(classes, "character"))
+    purrr::map_lgl(classes, function(class) methods::is(x, class)) %>% any()
 }
 is_any_of <- is_one_of
 
-# check if object is all of a vector classes
+# check if object all of a vector classes
 is_all_of <- function(x, classes) {
-    stopifnot(is(classes, "character"))
-    purrr::map_lgl(classes, function(class) is(x, class)) %>% all()
+    stopifnot(methods::is(classes, "character"))
+    purrr::map_lgl(classes, function(class) methods::is(x, class)) %>% all()
 }
 
 all_unique <- function(x) {
