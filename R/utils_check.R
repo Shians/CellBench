bf295d027a4b78c294a5039be92b9dde22887062
Shians <registertonysu@gmail.com>
2018-11-29 23:50:03 +1100

    Fixed is_all_of

--- R/utils_check.R
+++ R/utils_check.R
@@ -8,7 +8,7 @@ is_any_of <- is_one_of
 # check if object is all of a vector classes
 is_all_of <- function(x, classes) {
     stopifnot(is(classes, "character"))
-    purrr::map_lgl(classes, function(class) is(x, class)) %>% any()
+    purrr::map_lgl(classes, function(class) is(x, class)) %>% all()
 }
 
 all_unique <- function(x) {
