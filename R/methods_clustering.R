46efe11f9e8fdc80f855dd04d1bd4b96441db8c8
Shians <registertonysu@gmail.com>
2018-12-06 14:41:11 +1100

    Removed Seurat dependency until hdf5 requirements are resolved

--- R/methods_clustering.R
+++ R/methods_clustering.R
@@ -53,58 +53,58 @@ clustering_sc3 <- function(sce, col.sym = "Symbol") {
     return(factor(res))
 }
 
-#' Clustering using Seurat
-#' @inheritParams clustering_raceid
-#' @importMethodsFrom SingleCellExperiment counts
-#' @export
-clustering_seurat <- function(sce) {
-    num_dim <- floor(nrow(sce)/5000)
-    if (num_dim > 5) {
-        num_dim <- 5
-    }
+# #' Clustering using Seurat
+# #' @inheritParams clustering_raceid
+# #' @importMethodsFrom SingleCellExperiment counts
+# #' @export
+# clustering_seurat <- function(sce) {
+#     num_dim <- floor(nrow(sce)/5000)
+#     if (num_dim > 5) {
+#         num_dim <- 5
+#     }
 
-    # rough guess of how many PCA components to use
-    num_dim <- c(10, 10, 20, 30, 40, 50)[num_dim + 1]
+#     # rough guess of how many PCA components to use
+#     num_dim <- c(10, 10, 20, 30, 40, 50)[num_dim + 1]
 
-    pbmc <- Seurat::CreateSeuratObject(
-        raw.data = as.matrix(counts(sce)),
-        min.cells = 3,
-        min.genes = 0,
-        project = "",
-        display.progress = FALSE
-    )
+#     pbmc <- Seurat::CreateSeuratObject(
+#         raw.data = as.matrix(counts(sce)),
+#         min.cells = 3,
+#         min.genes = 0,
+#         project = "",
+#         display.progress = FALSE
+#     )
 
-    pbmc <- Seurat::NormalizeData(object = pbmc, display.progress = FALSE)
+#     pbmc <- Seurat::NormalizeData(object = pbmc, display.progress = FALSE)
 
-    pbmc <- Seurat::FindVariableGenes(
-        object = pbmc,
-        x.low.cutoff = 0.0125,
-        x.high.cutoff = 3,
-        y.cutoff = 0.5,
-        display.progress = FALSE
-    )
+#     pbmc <- Seurat::FindVariableGenes(
+#         object = pbmc,
+#         x.low.cutoff = 0.0125,
+#         x.high.cutoff = 3,
+#         y.cutoff = 0.5,
+#         display.progress = FALSE
+#     )
 
-    pbmc <- Seurat::ScaleData(
-        object = pbmc,
-        vars.to.regress = c("nUMI"),
-        display.progress = FALSE,
-        do.cpp = TRUE
-    )
+#     pbmc <- Seurat::ScaleData(
+#         object = pbmc,
+#         vars.to.regress = c("nUMI"),
+#         display.progress = FALSE,
+#         do.cpp = TRUE
+#     )
 
-    pbmc <- Seurat::RunPCA(
-        object = pbmc,
-        pcs.compute = num_dim,
-        pc.genes = pbmc@var.genes,
-        do.print = FALSE
-    )
+#     pbmc <- Seurat::RunPCA(
+#         object = pbmc,
+#         pcs.compute = num_dim,
+#         pc.genes = pbmc@var.genes,
+#         do.print = FALSE
+#     )
 
-    pbmc <- Seurat::FindClusters(
-        object = pbmc,
-        dims.use = 1:num_dim,
-        print.output = FALSE
-    )
+#     pbmc <- Seurat::FindClusters(
+#         object = pbmc,
+#         dims.use = 1:num_dim,
+#         print.output = FALSE
+#     )
 
-    res <- pbmc@meta.data$res.0.8
+#     res <- pbmc@meta.data$res.0.8
 
-    return(factor(res))
-}
+#     return(factor(res))
+# }
