9f98a17f3a578fae28a26f6c6deebbf9b071e9ea
Shians <registertonysu@gmail.com>
2018-12-12 16:58:39 +1100

    Added the ability to use the keep_ family of functions with matrices

--- R/utils_filter.R
+++ R/utils_filter.R
@@ -13,20 +13,36 @@ filter_zero_genes <- function(x) {
 
 # filter down to highest expressed genes
 keep_high_count_genes <- function(x, n) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    highest <- rowSums(SingleCellExperiment::counts(x)) %>%
+    stopifnot(is_one_of(x, c("SingleCellExperiment", "matrix")))
+
+    if (is(x, "SingleCellExperiment")) {
+        counts <- SingleCellExperiment::counts(x)
+    } else {
+        counts <- x
+    }
+
+    highest <- rowSums(counts) %>%
         order(decreasing = TRUE) %>%
-        subset_inds(seq_len(n))
+        subset_inds(seq_len(n)) %>%
+        sort()
 
     x[highest, ]
 }
 
 # filter down to largest samples
 keep_high_count_cells <- function(x, n) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    highest <- colSums(SingleCellExperiment::counts(x)) %>%
+    stopifnot(is_one_of(x, c("SingleCellExperiment", "matrix")))
+
+    if (is(x, "SingleCellExperiment")) {
+        counts <- SingleCellExperiment::counts(x)
+    } else {
+        counts <- x
+    }
+
+    highest <- colSums(counts) %>%
         order(decreasing = TRUE) %>%
-        subset_inds(seq_len(n))
+        subset_inds(seq_len(n))  %>%
+        sort()
 
     x[, highest]
 }
@@ -34,8 +50,14 @@ keep_high_count_cells <- function(x, n) {
 #' @importFrom stats var
 # filter down to highest expressed genes
 keep_high_var_genes <- function(x, n) {
-    stopifnot(is(x, "SingleCellExperiment"))
-    counts <- SingleCellExperiment::counts(x)
+    stopifnot(is_one_of(x, c("SingleCellExperiment", "matrix")))
+
+    if (is(x, "SingleCellExperiment")) {
+        counts <- SingleCellExperiment::counts(x)
+    } else {
+        counts <- x
+    }
+
     scaled_var <- row_apply(counts, stats::var) / rowSums(counts)
     highest <- scaled_var %>%
         order(decreasing = TRUE) %>%
