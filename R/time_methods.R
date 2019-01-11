8268e950ca880910f59507178a359f45f7096e35
Shians <registertonysu@gmail.com>
2019-01-11 11:55:19 +1100

    Changed ".name" arguments to "name"

--- R/time_methods.R
+++ R/time_methods.R
@@ -34,7 +34,7 @@
 #' time_methods(datasets, transform) %>%
 #'     unpack_timing() # extract timings out of list
 #'
-time_methods <- function(x, fn_list, .name = NULL, suppress.messages = TRUE) {
+time_methods <- function(x, fn_list, name = NULL, suppress.messages = TRUE) {
     method_names <- names(fn_list)
     if (length(method_names) != length(fn_list)) {
         stop("every element of fn_list must be named")
@@ -54,16 +54,16 @@ time_methods.list <- function(
     data_names <- names(x)
     method_names <- names(fn_list)
 
-    if (is.null(.name)) {
-        .name <- deparse(substitute(fn_list))
+    if (is.null(name)) {
+        name <- deparse(substitute(fn_list))
     }
 
     output <- make_combinations(data_names, method_names)
-    colnames(output) <- c("data", .name)
+    colnames(output) <- c("data", name)
 
     tasks <- purrr::map2(
         output$data,
-        output[[.name]],
+        output[[name]],
         function(dname, fname) {
             list(
                 data = x[[dname]],
@@ -86,7 +86,7 @@ time_methods.list <- function(
     output <- tibble::add_column(output, timed_result = timed_result)
 
     output$data <- factor_no_sort(output$data)
-    output[[.name]] <- factor_no_sort(output[[.name]])
+    output[[name]] <- factor_no_sort(output[[name]])
 
     class(output) <- c("benchmark_timing_tbl", class(output))
 
@@ -106,8 +106,8 @@ time_methods.benchmark_timing_tbl <- function(
 
     method_names <- names(fn_list)
 
-    if (missing(".name")) {
-        .name <- deparse(substitute(fn_list))
+    if (missing("name")) {
+        name <- deparse(substitute(fn_list))
     }
 
     output <- make_combinations(x, method_names)
@@ -140,7 +140,7 @@ time_methods.benchmark_timing_tbl <- function(
 
     output <- x %>% dplyr::select(-"timed_result")
     output <- tidyr::crossing(output, factor_no_sort(method_names))
-    names(output)[ncol(output)] <- .name
+    names(output)[ncol(output)] <- name
     output <- output %>%
         tibble::add_column(timed_result = results)
 
