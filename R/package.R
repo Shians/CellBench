f24f47824951cbcd7c2741ab04b05934130fac97
Shians <registertonysu@gmail.com>
2019-02-06 17:26:39 +1100

    Updated documentation

---
+++ R/package.R
@@ -0,0 +1,8 @@
+#' A framework for benchmarking combinations of methods
+#'
+#' @docType package
+#' @name CellBench-package
+#' @aliases CellBench
+#' @author Shian Su <\url{https://www.github.com/shians}>
+#' @seealso The core function in this package: \code{\link{apply_methods}}
+NULL
