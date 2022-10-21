ae1149df74dd20951175b7c317e85364d0eb47f7
Shians <registertonysu@gmail.com>
2022-10-21 14:47:45 +1100

    Updated docs

--- R/cellbench_casestudy.R
+++ R/cellbench_casestudy.R
@@ -1,11 +1,15 @@
 #' Open vignetted containing a case study using CellBench
 #'
-#' @export
+#' @return opens a vignette containing a case study
 #'
 #' @examples
 #' \dontrun{
 #' cellbench_case_study()
 #' }
+#'
+#' @importFrom utils browseURL
+#'
+#' @export
 cellbench_case_study <- function() {
     browseURL(system.file("case-study", "CellBenchCaseStudy.html", package = "CellBench"))
 }
